0e761c12f46db89b1a8d6e6b7c23ff9b88f54099
Udimberto Silvestre Junior <elvitous@gmail.com>
2015-11-01 23:11:58 -0200

    Ajustando módulo web específico para administração

---
+++ DeC-adm/web/js/dibrecommerce.js
@@ -0,0 +1,8 @@
+// VARIAVEIS PARA USO DE GAMBIARRAS
+var telaLargura = window.innerWidth;
+var telaAltura = window.innerHeight;
+window.addEventListener("resize", function () {
+  telaLargura = window.innerWidth;
+  telaAltura = window.innerHeight;
+});
+
