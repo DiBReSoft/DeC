5e577c7cfcd59a2f02b1fd3cd0fd2301bb3b4adf
fabioernanni <>
2015-11-22 23:34:25 -0200

    Correção da interação de data no índice

--- DeC-war/web/js/index-reservas.js
+++ DeC-war/web/js/index-reservas.js
@@ -11,17 +11,24 @@ $(document).ready(function () {
   /* Desabilita os campos inicialmente */
   $(".reservaCheckOut").attr("disabled", "disabled");
   /* Desabilita os campos inicialmente */
-
+  
+  //Configuração datepicker data checkin
   $(".reservaCheckIn").datepicker({
     language: "pt-BR",
-    format: "dd/mm/yyyy",
-    startDate: new Date()
-  });
-
-  $(".reservaCheckIn").on('blur', function () {
-
+    format: "dd/mm/yyyy"
+  }).datepicker('setStartDate', 'today');
+  
+  //Comportamento data checkin
+  $(".reservaCheckIn").datepicker().on('changeDate', function () {
+    $(".reservaCheckIn").datepicker('hide');
     if ($(".reservaCheckIn").val()) {
-
+      /*
+      var dataCheckIn = ($(".reservaCheckIn").val());
+      var dataCheckInDia = dataCheckIn.getDay();
+      var dataCheckInMes = dataCheckIn.getMonth();
+      var dataCheckInAno = dataCheckIn.getYear();
+      dataCheckIn = new Date(dataCheckInAno, dataCheckInMes, dataCheckInDia);
+      */
       var dataCheckIn = $(".reservaCheckIn").val();
       var dataCheckInDia = "" + dataCheckIn.charAt(0) + dataCheckIn.charAt(1);
       var dataCheckInMes = "" + dataCheckIn.charAt(3) + dataCheckIn.charAt(4) - 1;
@@ -29,22 +36,26 @@ $(document).ready(function () {
       dataCheckIn = new Date(dataCheckInAno, dataCheckInMes, dataCheckInDia);
       dataCheckIn.setDate(dataCheckIn.getDate() + 1);
       console.log(dataCheckIn);
-
-      $(".reservaCheckOut").datepicker({
+      
+      //Comportamento data checkout e configuração datepicker
+      $(".reservaCheckOut").focus().datepicker({
         language: "pt-BR",
-        format: "dd/mm/yyyy",
-        startDate: dataCheckIn
-      });
-
-      $(".reservaCheckOut").removeAttr("disabled");
-
+        format: "dd/mm/yyyy"
+      }).datepicker('setStartDate', ($(".reservaCheckIn").val() + 1)).show();
+      $(".reservaCheckOut").removeAttr('disabled');
+      $(".form-control.reservaCheckOut").focus();
+      
+      //Ao selecionar data checkout focus na categoria
+      $(".reservaCheckOut").datepicker().on('changeDate', function(){
+        $(this).datepicker('hide');
+        $(".reservaCategoria").focus();
+      }); 
     }
-
   });
 
   $(".reservaForm").on('submit', function () {
 
-    $(".reservaCheckOut").removeAttr("disabled");
+    $(".reservaCheckOut").removeAttr('disabled');
 
   });
 
