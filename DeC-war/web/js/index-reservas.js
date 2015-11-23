1c979961f450e1f207a2d36e3287528e14a0cfb2
fabio <fabio@DESKTOP-CS62PJM>
2015-11-23 00:17:53 -0200

    Correção do tratamento de validação dos campos

--- DeC-war/web/js/index-reservas.js
+++ DeC-war/web/js/index-reservas.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
 
   /* Desabilita os campos inicialmente */
   $(".reservaCheckOut").attr("disabled", "disabled");
+  $(".reservaCategoria").attr("disabled", "disabled");
+  $("input.btn[value='RESERVAR']").attr('disabled', 'disabled');
   /* Desabilita os campos inicialmente */
   
   //Configuração datepicker data checkin
@@ -21,22 +23,7 @@ $(document).ready(function () {
   //Comportamento data checkin
   $(".reservaCheckIn").datepicker().on('changeDate', function () {
     $(".reservaCheckIn").datepicker('hide');
-    if ($(".reservaCheckIn").val()) {
-      /*
-      var dataCheckIn = ($(".reservaCheckIn").val());
-      var dataCheckInDia = dataCheckIn.getDay();
-      var dataCheckInMes = dataCheckIn.getMonth();
-      var dataCheckInAno = dataCheckIn.getYear();
-      dataCheckIn = new Date(dataCheckInAno, dataCheckInMes, dataCheckInDia);
-      */
-      var dataCheckIn = $(".reservaCheckIn").val();
-      var dataCheckInDia = "" + dataCheckIn.charAt(0) + dataCheckIn.charAt(1);
-      var dataCheckInMes = "" + dataCheckIn.charAt(3) + dataCheckIn.charAt(4) - 1;
-      var dataCheckInAno = "" + dataCheckIn.charAt(6) + dataCheckIn.charAt(7) + dataCheckIn.charAt(8) + dataCheckIn.charAt(9);
-      dataCheckIn = new Date(dataCheckInAno, dataCheckInMes, dataCheckInDia);
-      dataCheckIn.setDate(dataCheckIn.getDate() + 1);
-      console.log(dataCheckIn);
-      
+    if ($(".reservaCheckIn").val()) {      
       //Comportamento data checkout e configuração datepicker
       $(".reservaCheckOut").focus().datepicker({
         language: "pt-BR",
@@ -47,16 +34,44 @@ $(document).ready(function () {
       
       //Ao selecionar data checkout focus na categoria
       $(".reservaCheckOut").datepicker().on('changeDate', function(){
+        $(".reservaCategoria").removeAttr('disabled', 'disabled');
         $(this).datepicker('hide');
         $(".reservaCategoria").focus();
       }); 
     }
   });
+  $(".reservaCategoria").on('change', function(){
+    if (this.selectedIndex !== 0)
+      $("input.btn[value='RESERVAR']").removeAttr('disabled', 'disabled');
+  });
 
   $(".reservaForm").on('submit', function () {
+    //Setar informações de checkin no formato UTM
+    var dataCheckIn = $(".reservaCheckIn").val();
+    var dataCheckInDia = "" + dataCheckIn.charAt(0) + dataCheckIn.charAt(1);
+    var dataCheckInMes = "" + dataCheckIn.charAt(3) + dataCheckIn.charAt(4) - 1;
+    var dataCheckInAno = "" + dataCheckIn.charAt(6) + dataCheckIn.charAt(7) + dataCheckIn.charAt(8) + dataCheckIn.charAt(9);
+    dataCheckIn = new Date(dataCheckInAno, dataCheckInMes, dataCheckInDia);
+    dataCheckIn.setDate(dataCheckIn.getDate() + 1);
+    
+    //Setar informações de checkout no formato UTM
+    var dataCheckOut = $(".reservaCheckIn").val();
+    var dataCheckInDia = "" + dataCheckOut.charAt(0) + dataCheckOut.charAt(1);
+    var dataCheckInMes = "" + dataCheckOut.charAt(3) + dataCheckOut.charAt(4) - 1;
+    var dataCheckInAno = "" + dataCheckOut.charAt(6) + dataCheckOut.charAt(7) + dataCheckOut.charAt(8) + dataCheckOut.charAt(9);
+    dataCheckOut = new Date(dataCheckInAno, dataCheckInMes, dataCheckInDia);
+    dataCheckOut.setDate(dataCheckOut.getDate() + 1);
 
+    console.log(dataCheckIn+" ate "+dataCheckOut.value);
     $(".reservaCheckOut").removeAttr('disabled');
 
   });
+      /* teste de outra forma para coletar o valor da data UTM
+      var dataCheckIn = new Date(Date.parse(($(".reservaCheckIn").val())));
+      var dataCheckInDia = dataCheckIn.getDay();
+      var dataCheckInMes = dataCheckIn.getMonth();
+      var dataCheckInAno = dataCheckIn.getYear();
+      dataCheckIn = new Date(dataCheckInAno, dataCheckInMes, dataCheckInDia);
+      */
 
 });
